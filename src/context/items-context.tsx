99c6fe8d61845370ade47e68680c5267a35d902b
Ali Bayatpour <aliglbayatpour@gmail.com>
2023-08-28 00:23:44 +0300

    examples

--- src/context/items-context.tsx
+++ src/context/items-context.tsx
@@ -17,20 +17,54 @@ interface Props {
   children: JSX.Element;
 }
 
+const AUTHORIZATION = `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIzMDc2YmM5Yy1mNDMzLTQzZmUtYTllNy0xYWY3MWY5NTAxN2IiLCJlbWFpbCI6Im9raGNoaTNAZ21haWwuY29tIiwiaWF0IjoxNjkzMTcwNjc0LCJleHAiOjE2OTMxNzQyNzR9.CIJpN8mYUQCEvNZ0V6tbtjGGDq8Lo8n5HOjsSb0_AAw`;
+
 export const ItemContextProvider: React.FC<Props> = ({ children }) => {
   const [items, setItems] = useState<IItem[] | []>(INITIAL_ITEMS.data);
+  const [error, setError] = useState<string>("");
 
   const onSetItems = (items: IItem[]) => {
     setItems(items);
   };
-  // const onfetch = () => {
-  //   const items = await get()
-  //   onSetItems(items)
-  // }
+
+  // const onfetch = async () => {
+  //   try {
+  //     const response = await fetch("URL_GOES_HERE", {
+  //       method: "get",
+  //       headers: new Headers({
+  //         Authorization: AUTHORIZATION,
+  //         "Content-Type": "application/json",
+  //       }),
+  //     });
+  //     const items = await response.json();
+  //     setErrorEmpty();
+  //     onSetItems(items);
+  //   } catch (error) {
+  //     setError("failed to fetch items");
+  //   }
+  // };
+
+  // const setErrorEmpty = () => {
+  //   setError("");
+  // };
+
   // const onUpdateItem = async () => {
-  //   await put(URL);
-  //   onfetch()
-  // }
+  //   try {
+  //     const response = await fetch("URL_GOES_HERE", {
+  //       method: "put",
+  //       headers: new Headers({
+  //         Authorization: AUTHORIZATION,
+  //         "Content-Type": "application/json",
+  //       }),
+  //       body: { ITEM_HERE },
+  //     });
+
+  //     onSetItems(items);
+  //     onfetch();
+  //   } catch (error) {
+  //     setError("failed to fetch items");
+  //   }
+  // };
 
   const contextValue = {
     items,
